267021ac354c2980531ef92ec8ed5a63d97f03a5
Jyothesh Karnam <96298283+jyotheshkar@users.noreply.github.com>
2024-05-16 18:00:20 +0100

    Update unit_test_DT.py

--- Unit and Integration Testing/unit_test_DT.py
+++ Unit and Integration Testing/unit_test_DT.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import patch, MagicMock
-import DT  # Ensure this matches the actual name of your script file
+import DT  
 from datetime import datetime
 
 class TestDTModule(unittest.TestCase):
